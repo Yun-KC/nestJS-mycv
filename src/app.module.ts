51b695944d148e1354d53f092d894c83c06781c1
YUN-KC <wjdrlfgg@gmail.com>
2022-06-22 20:31:34 +0900

    set typeorm in appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+/*
+sqlite는 파일 기반 데이터베이스입니다.
+데이터베이스와 관련된 모든 정보를 하나의 파일 안에 저장합니다.
+ */
